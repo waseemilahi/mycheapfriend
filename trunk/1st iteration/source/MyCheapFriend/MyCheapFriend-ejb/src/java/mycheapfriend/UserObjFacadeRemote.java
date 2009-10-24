adeef7c0a90c40fc47c56a6481f6e84c1c30e654
waseemilahi <waseemilahi@75b5836c-a9f6-11de-bea2-1db044c32224>
2009-10-24 03:04:02 +0000

    Updated Project. Does Include the three entities. (However, the unique ids need to be set)

---
+++ trunk/1st iteration/source/MyCheapFriend/MyCheapFriend-ejb/src/java/mycheapfriend/UserObjFacadeRemote.java
@@ -0,0 +1,25 @@
+/* The Session Bean Interface. */
+
+package mycheapfriend;
+
+import java.util.List;
+import javax.ejb.Remote;
+
+/**
+ *
+ * @author Waseem Ilahi
+ */
+@Remote
+public interface UserObjFacadeRemote {
+
+    void create(UserObj userObj);
+
+    void edit(UserObj userObj);
+
+    void remove(UserObj userObj);
+
+    UserObj find(long phone);
+
+    List<UserObj> findAll();
+
+}

4cb7ef52e9fdc83bd9c92c6de14aabb0df4c76ee
michael.glass <michael.glass@75b5836c-a9f6-11de-bea2-1db044c32224>
2009-10-29 06:28:45 +0000

    done with parser, need to integrate with shaoqing now

--- 1st iteration/source/ParserTester/src/java/mycheapfriend/Message.java
+++ 1st iteration/source/ParserTester/src/java/mycheapfriend/Message.java
@@ -5,6 +5,8 @@
 
 package mycheapfriend;
 
+import java.util.Vector;
+
 /**
  *
  * @author michaelglass
@@ -22,14 +24,8 @@ public class Message {
     }
 
     public boolean validBody() {
-        boolean rval = false;
-        //normalize phone numbers
-        // \d{10}|(?:\D?\d{3}\D ?(?:\d{7}|(?:\d{3}\D\d{4})))
-        String phoneNumPattern = "\\d{10}|(?:\\D?\\d{3}\\D ?(?:\\d{7}|(?:\\d{3}\\D\\d{4})))";
-
-        
-
-        return body.matches(phoneNumPattern);
+        String status = status();
+        return !status.startsWith("ERROR");
     }
     
     public void setBody(String body) {
@@ -70,6 +66,110 @@ public class Message {
 
     public void setTo(String to) {
         this.to = to;
+    }
+
+    private static String phoneNumPattern = "\\d{10}|(?:\\D?\\d{3}\\D ?(?:\\d{7}|(?:\\d{3}\\D\\d{4})))";
+    private static String nicknamePattern = "[a-zA-Z]{2,2}[a-zA-Z0-9_-]{1,8}";
+    private static String amountPattern = "\\$?\\d{1,4}(\\.\\d{2})?";
+
+    public String status()
+    {
+        body = body.trim();
+        if(body.length() == 0)
+            return body;
+        String[] atoms = body.split("\\s+");
+        String error_str = "ERROR: ";
+        boolean multiple_friends = false;
+        boolean error = false;
+
+
+        Vector<String> identifiers = new Vector<String>();
+        Vector<Integer> amounts = new Vector<Integer>();
+        
+        for(String atom : atoms)
+        {
+            atom = atom.trim();
+
+            if(atom.matches(nicknamePattern))
+                identifiers.add(atom.toLowerCase());
+            else if (atom.matches(phoneNumPattern))
+            {
+                identifiers.add(atom.replaceAll("\\D", ""));
+            }
+            else if (atom.matches(amountPattern))
+            {
+                atom = atom.replaceAll("[^\\d.]", "");
+                int ival = 0;
+                if(atom.indexOf('.') >= 0)
+                {
+                    atom = atom.replaceAll("\\.", "");
+                    ival = Integer.parseInt(atom);
+                }
+                else
+                {
+                    ival = Integer.parseInt(atom) * 100;
+                }
+                amounts.add(new Integer(ival));
+            }
+            else if(amounts.size() <= 1 && atom.equalsIgnoreCase("me"))
+            {
+                identifiers.add(atom);
+                multiple_friends = true;
+            }
+            else
+            {
+                error = true;
+                error_str += "unparsable atom: " + atom + "\n";
+                break;
+            }
+        }
+
+        if(identifiers.size() == 0 || amounts.size() == 0)
+        {
+            error = true;
+            error_str += "need at least one identifier and amount.\n";
+        }
+        else if(amounts.size() == identifiers.size() )
+        {
+            if(multiple_friends)
+            {
+                error = true;
+                error_str += "splitting a bill amongst multiple friends can only accept a \"me\" if you split a single bill between users\n";
+            }
+        }
+        else if (amounts.size() == 1 && identifiers.size() >= 2)
+        {
+            multiple_friends = true;
+        }
+        else
+        {
+            error = true;
+            error_str += "syntax error parsing request\n";
+        }
+
+        if(error)
+            return error_str;
+
+        else if(multiple_friends)
+        {
+            String rval = "Splitting " + amounts.firstElement() + " between the following friends: "
+                    + identifiers.remove(identifiers.size()-1);
+            
+            for(String ident : identifiers)
+            {
+                rval += ", " + ident;
+            }
+            return rval;
+        }
+        else
+        {
+            String rval = "";
+            for(int i = 0; i < identifiers.size(); i++)
+            {
+                rval += "Billing " + identifiers.elementAt(i) + " " + amounts.elementAt(i) + "\n";
+            }
+            return rval;
+        }
     }
 
 

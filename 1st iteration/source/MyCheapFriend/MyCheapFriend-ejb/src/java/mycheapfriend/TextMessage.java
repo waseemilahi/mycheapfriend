9db0a9dd79792ef4e4b160f9bbcaf19b7ab57093
michael.glass <michael.glass@75b5836c-a9f6-11de-bea2-1db044c32224>
2009-10-29 19:45:50 +0000

    modified controller, textMessage interface

--- 1st iteration/source/MyCheapFriend/MyCheapFriend-ejb/src/java/mycheapfriend/TextMessage.java
+++ 1st iteration/source/MyCheapFriend/MyCheapFriend-ejb/src/java/mycheapfriend/TextMessage.java
@@ -6,104 +6,40 @@
 package mycheapfriend;
 
 import java.util.List;
-import javax.annotation.Resource;
-import javax.jms.*;
 /**
  *
  * @author Shaoqing Niu
  */
-public class TextMessage {
+public interface TextMessage {
 
-    private int type; //defined in Global
-    private int errorType; //syntax error type
-    private long phone;    //347-244-6971
-    private String fromAddress; //address = 347-244-6971@att.com or other address
-                            //in case that people don't use cellphone to send msg
-                            //if not start with 9 digits, it's a syntax error
-    private String toAddress; //pass@cheapfriend.com will check whether password is for the phone
+//    private int type; //defined in Global
+//    private int errorType; //syntax error type
+//    private long phone;    //347-244-6971
+//    private String fromAddress; //address = 347-244-6971@att.com or other address
+//                            //in case that people don't use cellphone to send msg
+//                            //if not start with 9 digits, it's a syntax error
+//    private String toAddress; //pass@cheapfriend.com will check whether password is for the phone
+//
+//    private long friendPhone; //to set nickname for a friend
+//    private String friendNick;
+//
+//    private List<String> billFriend; //to request/settle a bill
+//    private List<Float> billMoney;
 
-    private long friendPhone; //to set nickname for a friend
-    private String friendNick;
+    public int getType();
+    public int getErrorType();
+    public long getPhone();
 
-    private List<String> billFriend; //to request/settle a bill
-    private List<Float> billMoney;
+    public String getFrom();
 
-    @Resource(mappedName="jms/FirstMessageFactory")
-    private  ConnectionFactory connectionFactory;
+    public String getDomain();
+    public String getPassword();
 
-    @Resource(mappedName="jms/FirstMessage")
-    private  Queue queue;
-
-    public void setType(int type){
-        this.type = type;
-    }
-
-    public int getType(){
-        return type;
-    }
-
-    public void setErrorType(int errorType){
-        this.errorType = errorType;
-    }
-
-    public int getErrorType(){
-        return errorType;
-    }
-
-    public void setPhone(int phone){
-        this.phone = phone;
-    }
-
-    public long getPhone(){
-        return phone;
-    }
-
-    public void setFromAddress(String fromAddress){
-        this.fromAddress = fromAddress;
-    }
-
-    public String getFromAddress(){
-        return fromAddress;
-    }
-
-    public void setToAddress(String toAddress){
-        this.toAddress = toAddress;
-    }
-
-    public String getToAddress(){
-        return toAddress;
-    }
-
-    public void setFriendPhone(long friendPhone){
-        this.friendPhone = friendPhone;
-    }
-
-    public long getFriendPhone(){
-        return friendPhone;
-    }
-
-    public void setFriendNick(String friendNick){
-        this.friendNick = friendNick;
-    }
-
-    public String getFriendNick(){
-        return friendNick;
-    }
-
-    public void addBillFriend(String billFriend){
-        this.billFriend.add(billFriend);
-    }
-
-    public String getBillFriend(int index){
-        return billFriend.get(index);
-    }
-
-    public void addBillMoney(float billMoney){
-        this.billMoney.add(billMoney);
-    }
-
-    public float getBillMoney(int index){
-        return billMoney.get(index);
-    }
+    public long getFriendPhone();
+    public String getFriendNick();
+    
+    public int getNumBills();
+    public Object getBillFriend(int index);
+    public long getBillMoney(int index);
 
 }

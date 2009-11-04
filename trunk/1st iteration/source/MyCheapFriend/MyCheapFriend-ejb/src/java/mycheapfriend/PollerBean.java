00fc1d33d23c219e91d822d5a5b5eeaec6df7819
waseemilahi <waseemilahi@75b5836c-a9f6-11de-bea2-1db044c32224>
2009-11-04 02:18:09 +0000

    Going to add the timer service....

---
+++ trunk/1st iteration/source/MyCheapFriend/MyCheapFriend-ejb/src/java/mycheapfriend/PollerBean.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package mycheapfriend;
+
+import javax.ejb.Stateless;
+
+/**
+ *
+ * @author Waseem Ilahi
+ */
+@Stateless
+public class PollerBean implements PollerRemote {
+
+    public void startTimer() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+    
+   
+ 
+}

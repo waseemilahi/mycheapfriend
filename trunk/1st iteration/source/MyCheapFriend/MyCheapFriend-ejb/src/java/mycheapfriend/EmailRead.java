4556e112daca9b5fc22b3a1d6b664112f085c4a4
David19860531 <David19860531@75b5836c-a9f6-11de-bea2-1db044c32224>
2009-11-04 01:44:56 +0000

    Change the return value of EmailRead.read() to Arraylist of EmailInfos

--- trunk/1st iteration/source/MyCheapFriend/MyCheapFriend-ejb/src/java/mycheapfriend/EmailRead.java
+++ trunk/1st iteration/source/MyCheapFriend/MyCheapFriend-ejb/src/java/mycheapfriend/EmailRead.java
@@ -1,4 +1,9 @@
 package mycheapfriend;
+
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author David
@@ -6,35 +11,30 @@ package mycheapfriend;
 
 
 public class EmailRead {
-
     /** Creates a new instance of Main */
-    public static void read() {
-         try {
-
-            GmailUtilities gmail = new GmailUtilities();
+    public static ArrayList<EmailInfo> read() throws Exception {
+            GmailUtilities gmail= new GmailUtilities();
             gmail.setUserPass("robot@mycheapfriend.com", "K86924");
             gmail.connect();
             gmail.openFolder("INBOX");
             //set all information...
             gmail.setEmailInfo();
-            System.out.println("# of new emails is " + gmail.getInfo().size());
+            System.out.println("# of new emails is " + gmail.info.size());
+            System.out.println(gmail.info.get(0).getTo());
             /* for(emailinfo a: gmail.getInfo())
                 System.out.println(a.getContent()); */
             gmail.closeFolder();
             gmail.disconnect();
-        } catch(Exception e) {
-            e.printStackTrace();
-            System.exit(-1);
-        }
-
-
+            return gmail.info;
     }
 
     /* test program for reading emails. */
     public static void main(String[] args) {
-        EmailRead.read();
-
-
+        try {
+            EmailRead.read();
+        } catch (Exception ex) {
+            Logger.getLogger(EmailRead.class.getName()).log(Level.SEVERE, null, ex);
+        }
 
     }
 

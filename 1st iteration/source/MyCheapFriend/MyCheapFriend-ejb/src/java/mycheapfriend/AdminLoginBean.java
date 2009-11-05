be85f4c4f6fca5419760ea43bb01cf1edea13ce4
waseemilahi <waseemilahi@75b5836c-a9f6-11de-bea2-1db044c32224>
2009-11-05 22:27:34 +0000

    Stateful Session Bean For Administrator Login (Empty)

---
+++ 1st iteration/source/MyCheapFriend/MyCheapFriend-ejb/src/java/mycheapfriend/AdminLoginBean.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package mycheapfriend;
+
+import javax.ejb.Stateful;
+
+/**
+ *
+ * @author Waseem Ilahi
+ */
+@Stateful
+public class AdminLoginBean implements AdminLoginRemote, AdminLoginLocal {
+    
+    
+    
+}
---
+++ 1st iteration/source/MyCheapFriend/MyCheapFriend-ejb/src/java/mycheapfriend/AdminLoginLocal.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package mycheapfriend;
+
+import javax.ejb.Local;
+
+/**
+ *
+ * @author Waseem Ilahi
+ */
+@Local
+public interface AdminLoginLocal {
+    
+}

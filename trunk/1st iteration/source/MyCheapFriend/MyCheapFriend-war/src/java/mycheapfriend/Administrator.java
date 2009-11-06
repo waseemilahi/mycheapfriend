8f068e0ac4f00dc574afa8565f2f402b06b8e98c
niushaoqing <niushaoqing@75b5836c-a9f6-11de-bea2-1db044c32224>
2009-11-06 11:06:55 +0000

    seems i committed a wrong version last night

---
+++ trunk/1st iteration/source/MyCheapFriend/MyCheapFriend-war/src/java/mycheapfriend/Administrator.java
@@ -0,0 +1,106 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package mycheapfriend;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.ejb.EJB;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Shaoqing Niu
+ */
+public class Administrator extends HttpServlet {
+
+    @EJB(mappedName="ejb.PollerBean")
+    private PollerRemote poller;
+    /** 
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code> methods.
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+        try {
+            // TODO output your page here
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Administrator</title>");  
+            out.println("</head>");
+            out.println("<body>");
+            if(poller.testStarted()) {
+                out.print("The server is running.");
+                out.println("<a href='StopService'>[stop]</a>");
+            }
+            else {
+                out.print("The server is stopped.");
+                out.println("<a href='StartService'>[start]</a>");
+            }
+            out.println("<br>");
+            //out.println("<a href='CreateUser'>Create a User.</a>");
+            //out.println("<br>");
+            //out.println("<a href='DeleteUserd'>Delete a User.</a>");
+            //out.println("<br>");
+            out.println("<a href='ListUsers'>List all Users.</a>");
+            out.println("<br>");
+            out.println("</body>");
+            out.println("</html>");
+            
+        } finally { 
+            out.close();
+        }
+    } 
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /** 
+     * Handles the HTTP <code>GET</code> method.
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+        processRequest(request, response);
+    } 
+
+    /** 
+     * Handles the HTTP <code>POST</code> method.
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /** 
+     * Returns a short description of the servlet.
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
